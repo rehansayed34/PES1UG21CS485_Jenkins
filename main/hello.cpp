3d497421edee0fb16525b51cbab49ea4c4b5b2a2
Rehan Sayed <99082235+rehansayed34@users.noreply.github.com>
2024-03-09 19:11:18 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "Hello, Rehan here\n";
+  cout << "Hello, Rehan Sayed here\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
